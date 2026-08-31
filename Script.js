11410820e46d9d5956714acf05831857a84a55b8
Briancollab <Brianstiven608@gmail.com>
2026-09-01 00:19:46 +0200

    Add event listener to button for user input

--- Script.js
+++ Script.js
@@ -1,1 +1,14 @@
+let boton = document.getElementById("generar");
 
+boton.addEventListener("click", async function() {
+
+    let caja = document.getElementById("peticion");
+    let resultado = document.getElementById("resultado");
+
+    let texto = caja.value;
+
+    resultado.textContent = "Dorrón está pensando... 🤖";
+
+    console.log("Petición del usuario:", texto);
+
+});
